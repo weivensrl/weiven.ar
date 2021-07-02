371cf6a81980e22facdb203a4c32cf2daf3cbbda
santuan <smrcouto88@gmail.com>
2021-07-02 14:52:32 -0300

    fix intro

--- components/Plugs/Intro.js
+++ components/Plugs/Intro.js
@@ -6,10 +6,10 @@ export default function About() {
   return (
     <div className={styles.container}>
       <div className="relative z-10 flex flex-col items-start justify-center w-full max-w-5xl px-6 mx-auto text-left md:px-0">
-        <h1 className="w-full mx-auto mb-3 text-6xl text-gray-100 ">
+        <h1 className="w-full mx-auto mb-3 text-2xl text-gray-100 md:text-6xl ">
           Simplificamos <br/> la tecnología
         </h1>
-        <h2 className="w-full max-w-md my-3 text-2xl text-left text-gray-100">
+        <h2 className="w-full max-w-md my-3 text-xl text-left text-gray-100 md:text-2xl">
           Expertos en tecnología para negocios y organizaciones gubernamentales.
         </h2>
       </div>
