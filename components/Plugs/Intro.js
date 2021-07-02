42e15b3b2acd58a36c63353f8aa7bce82dee572a
santuan <smrcouto88@gmail.com>
2021-07-02 15:22:12 -0300

    fix intro

--- components/Plugs/Intro.js
+++ components/Plugs/Intro.js
@@ -6,7 +6,7 @@ export default function About() {
   return (
     <div className={styles.container}>
       <div className="relative z-10 flex flex-col items-start justify-center w-full max-w-5xl px-6 mx-auto text-left md:px-0">
-        <h1 className="w-full mx-auto mb-3 text-2xl text-gray-100 md:text-6xl ">
+        <h1 className="w-full mx-auto mb-3 text-3xl text-gray-100 md:text-6xl ">
           Simplificamos <br/> la tecnología
         </h1>
         <h2 className="w-full max-w-md my-3 text-xl text-left text-gray-100 md:text-2xl">
