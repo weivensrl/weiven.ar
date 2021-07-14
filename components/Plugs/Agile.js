bfef13e4419dc7337be0fa5ca31fd9146ef1f6e8
santuan <smrcouto88@gmail.com>
2021-07-14 20:34:26 -0300

    fadein

--- components/Plugs/Agile.js
+++ components/Plugs/Agile.js
@@ -5,7 +5,7 @@ export default function Agile() {
   return (
     <div className={styles.container}>
       <div className="px-4 py-24 text-blue-700 bg-fixed bg-blue-600 bg-opacity-90 pattern-diagonal-lines-lg">
-        <Fade delay={200} bottom duration={1200}>
+        <Fade delay={200} duration={1200}>
           <h3 className="mb-2 text-3xl text-center text-white ">Metodologías Ágiles</h3>
           <div className="max-w-xl mx-auto text-xl text-center text-white">
             En Weiven usamos métodos ágiles como SCRUM o Kanban{" "}
--- components/Plugs/Security.js
+++ components/Plugs/Security.js
@@ -5,7 +5,7 @@ export default function Security() {
   return (
     <div className={styles.container}>
       <div className="px-4 py-24 text-gray-900 bg-fixed bg-black bg-opacity-90 pattern-diagonal-lines-lg">
-        <Fade delay={200} bottom duration={1200}>
+        <Fade delay={200} duration={1200}>
           <h3 className="mb-2 text-3xl text-yellow-500 ">Seguridad informática</h3>
           <div className="max-w-xl mx-auto text-xl text-white">
             Contamos con los mejores profesionales del sector de la Seguridad Informática.
