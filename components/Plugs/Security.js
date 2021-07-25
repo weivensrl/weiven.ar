72c4fb6977ce808a20c3d34d2c9e9d3338837a6d
santuan <smrcouto88@gmail.com>
2021-07-25 19:00:04 -0300

    fix contact height

--- components/Plugs/Security.js
+++ components/Plugs/Security.js
@@ -2,6 +2,7 @@ import styles from "@/components/Plugs/Security.module.css"
 import Fade from "react-reveal/Fade"
 import { IoIosArrowDown } from "react-icons/io"
 import AnchorLink from "react-anchor-link-smooth-scroll"
+import Link from "next/link"
 
 export default function Security() {
   return (
@@ -10,16 +11,15 @@ export default function Security() {
         <Fade delay={200} duration={1200}>
           <h3 className="mb-2 text-3xl text-yellow-500 ">Seguridad informática</h3>
           <div className="max-w-2xl mx-auto text-xl text-white">
-            Contamos con los mejores profesionales del sector de Seguridad.
-            Realizamos auditorias e implementaciones en Ciberseguridad.
+            Contamos con los mejores profesionales del sector de Seguridad. Realizamos
+            auditorias e implementaciones en Ciberseguridad.
           </div>
-          <AnchorLink
-            className="flex items-center justify-center py-1 mx-auto mt-6 font-sans text-lg font-bold text-gray-800 transition-all duration-500 transform bg-white border-b-2 border-transparent rounded shadow-md w-60 hover:border-yellow-500 hover:text-yellow-600"
-            href="#contact"
-          >
-            <span className="mr-3">Consultar</span>
-            <IoIosArrowDown className="animate-pulse "/>
-          </AnchorLink>
+          <Link href="/contacto">
+            <a className="flex items-center justify-center py-1 mx-auto mt-6 font-sans text-lg font-bold text-gray-800 transition-all duration-500 transform bg-white border-b-2 border-transparent rounded shadow-md w-60 hover:border-yellow-500 hover:text-yellow-600">
+              <span className="mr-3">Consultar</span>
+              <IoIosArrowDown className="transform -rotate-90 animate-pulse" />
+            </a>
+          </Link>
         </Fade>
       </div>
     </div>
--- components/Plugs/Services.js
+++ components/Plugs/Services.js
@@ -10,97 +10,73 @@ import {
 } from "react-icons/fc"
 import Fade from "react-reveal/Fade"
 
+
 export default function Services() {
   return (
     <div className={styles.container}>
       <div className="bg-white">
         <div className={styles.services}>
-          <Fade delay={200} duration={1200}>
-            <div className={styles.item}>
-              <div className="absolute top-0 right-0 mx-3 mt-2 kush-center opacity-80">
-                <FcMindMap className="mb-3 text-4xl" />
+          {services.map((service, i) => {
+            return (
+              <div key={i} className={styles.item}>
+                <Fade delay={service.delay} duration={1200}>
+                  <div className="absolute top-0 right-0 mx-3 mt-2 kush-center opacity-80">
+                    <span className="mb-3 text-4xl">{service.icon}</span>
+                  </div>
+                  <h3 className={styles.itemTitle}>{service.title}</h3>
+                  <p>{service.description}</p>
+                </Fade>
               </div>
-              <h3 className={styles.itemTitle}>Diseño de servicios</h3>
-              <h4 className={styles.subtitle}>Service Design</h4>
-              <p>
-                Tenemos la capacidad de transformar todos los procesos y servicios de una
-                empresa u organismo rediseñándolos en función de los usuarios internos y
-                externos a partir de la implementación tecnológica.{" "}
-              </p>
-            </div>
-          </Fade>
-          <Fade delay={400} duration={1200}>
-            <div className={styles.item}>
-              <div className="absolute top-0 right-0 mx-3 mt-2 kush-center opacity-80">
-                <FcSettings className="mb-3 text-4xl" />
-              </div>
-              <h3 className={styles.itemTitle}>Desarrollo de Software</h3>
-              <h4 className={styles.subtitle}>Software Factory</h4>
-              <p>
-                Brindamos soluciones en software y aplicaciones móviles. Somos partner de
-                las empresas líderes en el mercado pudiendo dar soluciones
-                con software licenciatario o software libre ofreciendo el soporte y
-                desarrollo de software propietario de manera personalizada.
-              </p>
-            </div>
-          </Fade>
-          <Fade delay={600} duration={1200}>
-            <div className={styles.item}>
-              <div className="absolute top-0 right-0 mx-3 mt-2 kush-center opacity-80">
-                <FcMultipleDevices className="mb-3 text-4xl" />
-              </div>
-              <h3 className={styles.itemTitle}>Experiencia de usuario</h3>
-              <h4 className={styles.subtitle}>UX/UI</h4>
-              <p>
-                Diseñamos experiencias de calidad según estándares internacionales y
-                buenas prácticas UX/IU. Contamos con servicios de aseguramiento de la
-                calidad: QA Web (CMS, aplicaciones web, diseño, etc.), QA Mobile
-                (aplicaciones móviles, responsive, user experience, etc.
-              </p>
-            </div>
-          </Fade>
-          <Fade delay={200} duration={1200}>
-            <div className={styles.item}>
-              <div className="absolute top-0 right-0 mx-3 mt-2 kush-center opacity-80">
-                <FcDataProtection className="mb-3 text-4xl" />
-              </div>
-              <h3 className={styles.itemTitle}>Soluciones en infraestructura</h3>
-              <p>
-                Disponemos de las mejores soluciones tecnológicas del mercado para que
-                nuestros clientes puedan desarrollarse y satisfacer todas sus necesidades.
-              </p>
-            </div>
-          </Fade>
-          <Fade delay={400} duration={1200}>
-            <div className={styles.item}>
-              <div className="absolute top-0 right-0 mx-3 mt-2 kush-center opacity-80">
-                <FcSafe className="mb-3 text-4xl" />
-              </div>
-              <h3 className={styles.itemTitle}>
-                Blockchain
-              </h3>
-              <p>
-                Expertos en la aplicación y desarrollo de proyectos blockchain,
-                criptomonedas e implementación de billeteras virtuales para gobiernos
-                en todos sus niveles (Municipales, Provinciales y Nacionales). Abordaje tecnológico práctico con utilidad real para la gestión e implementación de políticas públicas.
-              </p>
-            </div>
-          </Fade>
-          <Fade delay={600} duration={1200}>
-            <div className={styles.item}>
-              <div className="absolute top-0 right-0 mx-3 mt-2 kush-center opacity-80">
-                <FcOrganization className="mb-3 text-4xl" />
-              </div>
-              <h3 className={styles.itemTitle}>Smart Cities</h3>
-              <p>
-                Diseñamos planes estratégicos y desarrollamos productos para Smart Cities.
-                Movilidad urbana y car parking. Transformación digital de organismos
-                públicos (Municipales, provinciales y nacionales). Seguridad inteligente.
-              </p>
-            </div>
-          </Fade>
+            )
+          })}
         </div>
       </div>
     </div>
   )
 }
+
+
+const services = [
+  {
+    title: "Diseño de servicios",
+    description:
+      " Tenemos la capacidad de transformar todos los procesos y servicios de una empresa u organismo rediseñándolos en función de los usuarios internos y externos a partir de la implementación tecnológica.",
+    delay: 800,
+    icon: <FcMindMap />,
+  },
+  {
+    title: "Desarrollo de Software",
+    description:
+      "Brindamos soluciones en software y aplicaciones móviles. Somos partner de las empresas líderes en el mercado pudiendo dar soluciones con software licenciatario o software libre ofreciendo el soporte y desarrollo de software propietario de manera personalizada.",
+    delay: 1200,
+    icon: <FcSettings />,
+  },
+  {
+    title: "Experiencia de usuario",
+    description:
+      "Diseñamos experiencias de calidad según estándares internacionales y buenas prácticas UX/IU. Contamos con servicios de aseguramiento de la calidad: QA Web (CMS, aplicaciones web, diseño, etc.), QA Mobile (aplicaciones móviles, responsive, user experience, etc.",
+    delay: 1400,
+    icon: <FcMultipleDevices />,
+  },
+  {
+    title: "Soluciones en infraestructura",
+    description:
+      "Disponemos de las mejores soluciones tecnológicas del mercado para que nuestros clientes puedan desarrollarse y satisfacer todas sus necesidades.",
+    delay: 1600,
+    icon: <FcDataProtection />,
+  },
+  {
+    title: "Blockchain",
+    description:
+      "Expertos en la aplicación y desarrollo de proyectos blockchain, criptomonedas e implementación de billeteras virtuales para gobiernos en todos sus niveles (Municipales, Provinciales y Nacionales). Abordaje tecnológico práctico con utilidad real para la gestión e implementación de políticas públicas.",
+    delay: 1800,
+    icon: <FcSafe />,
+  },
+  {
+    title: "Smart Cities",
+    description:
+      "Diseñamos planes estratégicos y desarrollamos productos para Smart Cities. Movilidad urbana y car parking. Transformación digital de organismos públicos (Municipales, provinciales y nacionales). Seguridad inteligente.",
+    delay: 2200,
+    icon: <FcOrganization />,
+  },
+]
